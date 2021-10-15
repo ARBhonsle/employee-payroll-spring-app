8e4b320d7230bf683243da20dd27804b96924c35
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-10-15 18:25:51 +0530

    Added: Employee Exceptions and ExceptionHandler

---
+++ src/main/java/com/example/employeepayrollspringapp/exceptions/EmployeeException.java
@@ -0,0 +1,14 @@
+package com.example.employeepayrollspringapp.exceptions;
+
+/**
+ * EmployeeException
+ *  throws exceptions during execution of application
+ * @author Aditi
+ * @version 0.0.1
+ * @since 11-10-2021
+ */
+public class EmployeeException extends Exception {
+    public EmployeeException(String message){
+        super(message);
+    }
+}
